a9baa73ff86eadcf48d83a8b2e771746fc0cd8f4
cheeeeze <nyj9349@hanmail.net>
2022-07-22 14:26:25 +0900

    modify spring message properties

--- sp5-chap12/src/main/java/api/RegistAPIController.java
+++ sp5-chap12/src/main/java/api/RegistAPIController.java
@@ -4,9 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import spring.DuplicateMemberException;
 import spring.MemberRegistService;
 import spring.RegisterRequest;
-import util.RegisterRequestValidator;
 
 @Controller
 @RequestMapping( "/register")
