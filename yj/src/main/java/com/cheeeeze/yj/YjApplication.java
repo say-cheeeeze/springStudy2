59cdfbb391d601042782eb19c36d7e8c0ada652b
cheeeeze <nyj9349@hanmail.net>
2022-07-25 15:57:51 +0900

    [ add ] add springBoot Project

---
+++ yj/src/main/java/com/cheeeeze/yj/YjApplication.java
@@ -0,0 +1,13 @@
+package com.cheeeeze.yj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YjApplication.class, args);
+	}
+
+}
---
+++ yj/src/test/java/com/cheeeeze/yj/YjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheeeeze.yj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
